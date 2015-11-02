b1124a606a553aec378135e10c79eabd9848a1b7
Anton Gorshkov <antongorshkov12@gmail.com>
2015-11-03 01:03:47 +0800

    -

--- app/src/main/java/com/penpen/profview/clubs_nf_fragment.java
+++ app/src/main/java/com/penpen/profview/clubs_nf_fragment.java
@@ -20,19 +20,19 @@ public class clubs_nf_fragment extends nf_fragment {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getContext());
         List<String> urls = new ArrayList<>();
         if (settings.getBoolean("club_gos", false)) {
-            urls.add("https://api.vk.com/method/wall.get?owner_id=-65955842&filter=all&count=100");
+            urls.add("https://api.vk.com/method/wall.get?owner_id=-65955842&filter=all");
         }
         if (settings.getBoolean("club_sno", false)) {
-            urls.add("https://api.vk.com/method/wall.get?owner_id=-91188284&filter=all&count=100");
+            urls.add("https://api.vk.com/method/wall.get?owner_id=-91188284&filter=all");
         }
         if (settings.getBoolean("club_volonter", false)) {
-            urls.add("https://api.vk.com/method/wall.get?owner_id=-77276649&filter=all&count=100");
+            urls.add("https://api.vk.com/method/wall.get?owner_id=-77276649&filter=all");
         }
         if (settings.getBoolean("club_gost", false)) {
-            urls.add("https://api.vk.com/method/wall.get?owner_id=-61189805&filter=all&count=100");
+            urls.add("https://api.vk.com/method/wall.get?owner_id=-61189805&filter=all");
         }
         if (settings.getBoolean("club_kok", false)) {
-            urls.add("https://api.vk.com/method/wall.get?owner_id=-68602555&filter=all&count=100");
+            urls.add("https://api.vk.com/method/wall.get?owner_id=-68602555&filter=all");
         }
         return urls;
     }
